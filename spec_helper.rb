910fed1cff059ae0ab0af06f31e804f8ebaf2604
wangt <wangtaofighting@163.com>
2017-12-13 13:53:33 +0800

    add token

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,9 +1,10 @@
 require 'simplecov'
-require 'coveralls'
-require 'codeclimate-test-reporter'
 
-CodeClimate::TestReporter.start
-
-SimpleCov.start do
-  add_filter '/test/'
+# Check to see if we're inside a CI environment. If not, we don't want to override the default formatter.
+if ENV['CI']
+  require 'coveralls'
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 end
+
+# The NO_COVERAGE clause is optional - Coveralls and CircleCI don't use it by default, but may be useful for you in other circumstances.
+SimpleCov.start 'rails' unless ENV.key?('NO_COVERAGE')
